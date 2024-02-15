c45c41f4ca0817e5cb7031a94d166d67aab09ed3
unknown <karina.yukting.li@gmail.com>
2024-02-14 22:52:12 -0500

    Added mongodb connection

---
+++ config/connection.js
@@ -0,0 +1,8 @@
+const { connect, connection } = require('mongoose');
+
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/kbnetDB';
+
+connect(connectionString);
+
+module.exports = connection;
