2fb2f32d42f3d0f68b357f5f12d5b4a7e51f8ad6
unknown <karina.yukting.li@gmail.com>
2024-02-15 23:38:38 -0500

    Created controllers and routes files. Added comments on what need to be done according to requirements

---
+++ controllers/thoughtController.js
@@ -0,0 +1,9 @@
+const {Thought, User} = require('../../models');
+
+// GET all thoughts
+// GET a single thought by id
+// POST a new thought
+// PUT (update) a thought by id
+// DELETE a thought by id
+// POST a reaction to a thought's reactions array field
+// DELETE a reaction by the reaction's reactionId value
---
+++ controllers/userController.js
@@ -0,0 +1,10 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Thought } = require('../models');
+
+// GET all users
+// GET a single user by id
+// POST a new user
+// PUT (update) a user by id
+// DELETE a user by id
+// POST a new friend to a user's friend list
+// DELETE a friend from a user's friend list
