077efbf009541132dbe26e4f7d5af93fe913ec05
unknown <karina.yukting.li@gmail.com>
2024-02-21 00:19:30 -0500

    Added data for thoughts

---
+++ seeds/thoughtData.js
@@ -0,0 +1,7 @@
+const thoughts = [
+  { username: "Jane", thoughtText: "Mistakes are part of the learning process in coding; don't be discouraged by them." },
+  { username: "Bob", thoughtText: "Start with simple projects to build confidence and gradually tackle more complex tasks." },
+  { username: "Jill", thoughtText: "Don't rush through learning; take your time to understand the concepts thoroughly." },
+];
+
+module.exports = thoughts;
