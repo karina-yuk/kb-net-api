9b9372701ddf43581ccfcc05177c272553911556
unknown <karina.yukting.li@gmail.com>
2024-02-21 00:18:32 -0500

    Added data for users

---
+++ seeds/userData.js
@@ -0,0 +1,7 @@
+const users = [
+  { username: "Jane", email: "jane_25@gmail.com" },
+  { username: "Bob", email: "bob_82@gmail.com" },
+  { username: "Jill", email: "jill_123@gmail.com" },
+];
+
+module.exports = users;
