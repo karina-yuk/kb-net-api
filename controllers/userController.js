84b8c91259ac4ddef4ed6843350ef6705a21bf91
unknown <karina.yukting.li@gmail.com>
2024-02-21 00:03:55 -0500

    Added GET, POST, PUT, and DELETE functionality for users and POST and DELETE functionality for  adding and removing a friend from user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,100 @@
-const { ObjectId } = require('mongoose').Types;
-const { User, Thought } = require('../models');
+const { ObjectId } = require("mongoose").Types;
+// Importing the User and Thought models from the models directory
+const { User, Thought } = require("../models");
 
-// GET all users
-// GET a single user by id
-// POST a new user
-// PUT (update) a user by id
-// DELETE a user by id
-// POST a new friend to a user's friend list
-// DELETE a friend from a user's friend list
+module.exports = {
+  // GET all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // GET a single user by id
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+        .select("-__v")
+        .populate("thoughts")
+        .populate("friends");
+
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // POST a new user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // PUT (update) a user by id
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { new: true }
+      );
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // DELETE a user by id
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // POST a new friend to a user's friend list
+  async addFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.params.friendId } },
+        { new: true }
+      );
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // DELETE a friend from a user's friend list
+  async deleteFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendId } },
+        { new: true }
+      );
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
